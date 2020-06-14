79d7b98750990561e4ad32766aede00f73547fa9
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-14 20:01:33 +0000

    addint comments

--- sort.h
+++ sort.h
@@ -1,9 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
+/* HEADERS. */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* STRUCTURES. */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,8 +20,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* FUNCTIONS. */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+
 #endif /* SORT_H */
