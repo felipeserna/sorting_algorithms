bfed03cb98499bfaf541b54b2a81a1a1438bf2c7
Felipe Serna <feserna86@gmail.com>
2020-06-15 00:44:33 +0000

    added comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,19 +13,22 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		aux_2 = i;
-		for (j = i; j < size; j++)
+		aux_2 = i; /* new element to compare */
+		for (j = i; j < size; j++) /* searching smallest element */
 		{
 			if (array[aux_2] > array[j])
 			{
+				/* storing index of new smallest element */
 				aux_2 = j;
 			}
 		}
+		/* making the swap */
 		aux = array[i];
 		array[i] = array[aux_2];
 		array[aux_2] = aux;
 
 		if (array[aux_2] != array[i])
+			/* a swap was made */
 			print_array(array, size);
 	}
 }
