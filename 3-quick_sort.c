2ad77d87ee075ab88c82eeb8a4a21326e3533602
Felipe Serna <feserna86@gmail.com>
2020-06-16 15:37:24 +0000

    function low_high added

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,31 +8,53 @@
  */
 size_t partition(int *array, size_t size, size_t low, size_t high)
 {
-        size_t i = 0, j, aux, pivot = size - 1;
+	size_t i = 0, j, aux, pivot = size - 1;
 
-        for (j = 0; j < pivot; j++)
-        {
-                if (array[j] <= array[pivot] && array[i] > array[pivot])
-                {
-                        aux = array[i];
-                        array[i] = array[j];
-                        array[j] = aux;
-                        i++;
-                        print_array(array, size);
-                }
-                if (array[j] <= array[pivot] && array[i] <= array[pivot])
-                {
-                        i++;
-                }
-        }
-        if (array[pivot] < array[i])
-        {
-                aux = array[i];
-                array[i] = array[pivot];
-                array[pivot] = aux;
-                print_array(array, size);
-        }
-        return (i);
+	for (j = 0; j < pivot; j++)
+	{
+		if (array[j] <= array[pivot] && array[i] > array[pivot])
+		{
+			aux = array[i];
+			array[i] = array[j];
+			array[j] = aux;
+			i++;
+			print_array(array, size);
+		}
+		if (array[j] <= array[pivot] && array[i] <= array[pivot])
+		{
+			i++;
+		}
+	}
+	if (array[pivot] < array[i])
+	{
+		aux = array[i];
+		array[i] = array[pivot];
+		array[pivot] = aux;
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * low_high - sorts an array of integers in ascending
+ * order using the Quick sort algorithm.
+ * @array: array of integers
+ * @size: size of the array
+ * Return: void
+ */
+void low_high(int *array, size_t size, size_t low, size_t high)
+{
+	size_t pivot, low = 0, high;
+
+
+	if (size > 1 )
+	{
+		pivot = partition(array, size, low, high);
+		low = size - 1 - pivot;
+		/*printf("pivot is %d\n", (int)pivot);*/
+		quick_sort(array, pivot); /* left array */
+		quick_sort(array + pivot + 1, size - 1 - pivot); /* right */
+	}
 }
 
 /**
@@ -44,7 +66,7 @@ size_t partition(int *array, size_t size, size_t low, size_t high)
  */
 void quick_sort(int *array, size_t size)
 {
-        size_t low = 0, high = size - 1;
+	size_t low = 0, high = size - 1;
 
-        low_high(array, size, low, high);
+	low_high(array, size, low, high);
 }
