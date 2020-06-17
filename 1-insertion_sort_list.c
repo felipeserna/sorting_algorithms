31c7f4c8742cbf165c56bfd877e69ff7fca47115
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-17 19:07:00 +0000

    Changing the comparison of the limits of list.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,42 +6,26 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = (*list)->next, *aux_0 = NULL;
+	listint_t *head = (*list)->next, *tmp = NULL;
 
 	while (head != NULL)
 	{
 		while (head->prev && head->n < head->prev->n)
 		{
-			if (head->prev->prev)
-			{
-				aux_0 = head->prev->prev; /* save prev de 4 */
-				head->prev->prev->next = head; /* 0 a 1 */
-				if (head->next)
-				{
-					head->next->prev = head->prev;/*2a4 */
-					head->prev->next = head->next; /*4a2 */
-					head->next = head->prev; /* 1 a 4 */
-					head->prev->prev = head; /* 4 a 1 */
-					head->prev = aux_0;  /* 1 a 0 */
-				}
-				else
-				{
-					head->next = head->prev; /* 1 a 4 */
-					head->prev->prev = head; /* 4 a 1 */
-					head->prev->next = NULL;
-					head->prev = aux_0;  /* 1 a 0 */
-
-				}}
+			tmp = head->prev;
+			if (tmp->prev)
+				(tmp->prev)->next = head;
 			else
-			{
-				head->next->prev = head->prev;  /* 2 a 4 */
-				head->prev->next = head->next; /* 4 a 2 */
-				head->next = head->prev; /* 1 a 4 */
-				head->prev->prev = head; /* 4 a 1 */
-				head->prev = NULL;
 				*list = head;
-			}
+			head->prev = tmp->prev;
+			if (head->next)
+				(head->next)->prev = tmp;
+			tmp->next = head->next;
+			tmp->prev = head;
+			head->next = tmp;
+
 			print_list(*list);
 		}
 		head = head->next;
-	}}
+	}
+}
