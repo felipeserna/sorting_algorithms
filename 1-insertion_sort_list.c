160f852b91bb95430d7c7e567a5396c5b00b2974
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-17 20:04:47 +0000

    list validation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,6 +8,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head = (*list)->next, *tmp = NULL;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 	while (head != NULL)
 	{
 		while (head->prev && head->n < (head->prev)->n)
@@ -21,10 +23,8 @@ void insertion_sort_list(listint_t **list)
 			if (head->next)
 				(head->next)->prev = tmp;
 			tmp->next = head->next;
-			/*tmp->prev = head;*/
-			/*head->next = tmp;*/
-			head->next = tmp;
 			tmp->prev = head;
+			head->next = tmp;
 
 			print_list(*list);
 		}
