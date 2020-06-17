e40e8b684a341c90bd64a5eb51674b0280b35c1c
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-17 02:22:37 +0000

    if deleted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,11 +6,8 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = (*list)->next;
-	listint_t *aux_0 = NULL;
+	listint_t *head = (*list)->next, *aux_0 = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
 	while (head != NULL)
 	{
 		while (head->prev && head->n < head->prev->n)
