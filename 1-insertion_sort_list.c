79a6b08b6f3a00957218b6a85b38980f64bd3650
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-17 02:19:24 +0000

    checking if list exist

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,6 +8,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head = (*list)->next;
 	listint_t *aux_0 = NULL;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	while (head != NULL)
